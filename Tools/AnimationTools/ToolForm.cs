d7a5a5ddc8fe1c923ac383a7e0ae678e257b0f1d
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2024-05-16 13:58:07 -0700

    Gif Tools App Upgraded

--- Tools/AnimationTools/ToolForm.cs
+++ Tools/AnimationTools/ToolForm.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.Remoting.Messaging;
+//using System.Runtime.Remoting.Messaging;
 using static FileManager;
 using System.Diagnostics;
 
